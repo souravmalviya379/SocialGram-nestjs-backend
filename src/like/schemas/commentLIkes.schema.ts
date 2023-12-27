f5417c86f65d6b544a501e97a9e76ad942d53de5
souravmalviya379 <souravmalviyaji11@gmail.com>
2023-12-27 15:10:24 +0530

    add like feature for comment

---
+++ src/like/schemas/commentLIkes.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { CommentDocument } from 'src/comment/schemas/comment.schema';
+import { UserDocument } from 'src/user/schemas/user.schema';
+
+export type CommentLikesDocument = HydratedDocument<CommentLikes>;
+
+@Schema({ timestamps: true })
+export class CommentLikes {
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: UserDocument;
+
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+  })
+  comment: CommentDocument;
+}
+
+export const CommentLikesSchema = SchemaFactory.createForClass(CommentLikes);
